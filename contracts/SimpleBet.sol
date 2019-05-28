178ef05a29bcde6cb4ebd9d4c8d34314d03f9a49
WTM-Thinker <47021231+WTM-Thinker@users.noreply.github.com>
2019-05-28 16:48:10 +0800

    Update SimpleBet.sol

--- contracts/SimpleBet.sol
+++ contracts/SimpleBet.sol
@@ -1,7 +1,15 @@
-pragma soliditypp ^0.4.0;
+pragma soliditypp ^0.4.2;
 contract ViteBet{
     address owner;
-    tokenId token = "tti_5649544520544f4b454e6e40";
+
+    struct BetLimit {
+        uint256 lowerLimit;
+        uint256 upperLimit;
+        uint256 tipPer;
+    }
+
+    tokenId[] tokens = ["tti_5649544520544f4b454e6e40"];
+    mapping(tokenId => BetLimit) public tokenMap;
 
     event win(address indexed addr, uint256 rollTarget, uint256 betAmount, uint64 rollNum, uint256 winAmount);
     event lose(address indexed addr, uint256 rollTarget, uint256 betAmount, uint64 rollNum);
@@ -9,26 +17,54 @@ contract ViteBet{
 
     constructor() public {
         owner = msg.sender;
+        tokenMap["tti_5649544520544f4b454e6e40"].lowerLimit = 1 vite;
+        tokenMap["tti_5649544520544f4b454e6e40"].upperLimit = 100 vite;
+        tokenMap["tti_5649544520544f4b454e6e40"].tipPer = 5;
     }
 
     onMessage () payable {
     }
 
+    // 配置token赌注上下限
+    // 配置抽成比例（0到20）
+    onMessage configBetLimit(uint256 ll, uint256 ul, uint256 tp) {
+        require(owner == msg.sender);
+        require(ll > 0 && ll <= ul);
+        require(tp >= 0 && tp <= 20);
+        if (tokenMap[msg.tokenid].lowerLimit == 0)
+            tokens.push(msg.tokenid);
+        tokenMap[msg.tokenid].lowerLimit = ll;
+        tokenMap[msg.tokenid].upperLimit = ul;
+        tokenMap[msg.tokenid].tipPer = tp;
+    }
+
     onMessage DrawMoney(uint256 amount) {
         require(owner == msg.sender);
-        require(amount <= address(this).balance(token));
-        msg.sender.transfer(token, amount);
+        require(amount <= balance(msg.tokenid));
+        msg.sender.transfer(msg.tokenid, amount);
+    }
+
+    // 获取token的上下限和费率
+    getter getBetLimit(tokenId token) returns(uint256 ll, uint256 ul, uint256 tipPer) {
+        return (tokenMap[token].lowerLimit, tokenMap[token].upperLimit, tokenMap[token].tipPer);
+    }
+
+    // 获取token的列表
+    getter getTokenList() returns(tokenId[] memory) {
+        return tokens;
     }
 
     onMessage BetAndRoll(uint256 rollTargets) payable {
-        uint256 betAmount = msg.value;
+        uint256 betAmount = msg.amount;
         address betAddr = msg.sender;
-        require(msg.tokenid == token);
-        require(betAmount >= 1 vite && betAmount <= 100 vite);
+        uint256 ll = tokenMap[msg.tokenid].lowerLimit;
+        uint256 ul = tokenMap[msg.tokenid].upperLimit;
+        require(ll > 0 && ll <= ul);
+        require(betAmount >= ll && betAmount <= ul);
         require(rollTargets > 0 && rollTargets < 100000);
 
-        bytes32 randomhash = blockhash(block.number);
-        uint64 rollNum = uint64(uint256((randomhash<<232)>>232) % 6 + 1);
+        uint64 randomNumber = random64();
+        uint64 rollNum = randomNumber % 6 + 1;
         bool winBet = false;
         uint64 count = 0;
         uint256 tempRollTargets = rollTargets;
@@ -41,24 +77,24 @@ contract ViteBet{
             }
             tempRollTargets = tempRollTargets / 10;
         }
-        uint256 winAmount = calcWinAmount(betAmount, count);
+        uint256 winAmount = calcWinAmount(betAmount, count, msg.tokenid);
 
         if(winBet == false) {
-            betAddr.transfer(token, 0);
+            // betAddr.transfer(msg.tokenId, 0);
             emit lose(betAddr, rollTargets, betAmount, rollNum);
-        } else if(winBet == true && winAmount > address(this).balance(token)) {
-            betAddr.transfer(token, betAmount);
+        } else if(winBet == true && winAmount > balance(msg.tokenid)) {
+            betAddr.transfer(msg.tokenid, betAmount);
             emit suspendBet(betAddr, rollTargets, betAmount);
         } else {
-            betAddr.transfer(token, winAmount);
+            betAddr.transfer(msg.tokenid, winAmount);
             emit win(betAddr, rollTargets, betAmount, rollNum, winAmount);
         }
 
     }
 
-    function calcWinAmount(uint256 betAmount, uint256 length) public pure returns(uint256) {
+    function calcWinAmount(uint256 betAmount, uint256 length, tokenId token) public view returns(uint256) {
         uint256 bonus = betAmount * 6 / length;
-        return betAmount + (bonus - betAmount) * 95 / 100;
+        return betAmount + (bonus - betAmount) * (100 - tokenMap[token].tipPer) / 100;
     }
 
 }
